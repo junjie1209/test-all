4422e482ddcca8edd5e87fa53d2c92b39adaeccf
张俊杰 <zhangjunjie@nantian.com.cn>
2022-02-14 15:10:03 +0800

    feat: 同步代码

--- test-validation/src/main/java/com/nantian/test/validation/pojo/Person.java
+++ test-validation/src/main/java/com/nantian/test/validation/pojo/Person.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 /**
  * TODO
  *
@@ -14,4 +18,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
+    @NotNull(message = "classId 不能为空")
+    private String classId;
+
+    @Size(max = 33)
+    @NotNull(message = "name 不能为空")
+    private String name;
+
+    @Pattern(regexp = "((^Man$|^Woman$|^UGM$))", message = "sex 值不在可选范围")
+    @NotNull(message = "sex 不能为空")
+    private String sex;
+
+    @Email(message = "email 格式不正确")
+    @NotNull(message = "email 不能为空")
+    private String email;
+
 }
