4b6ec6870cbd2a88ae1f7ae4840bcee491ac3cce
张俊杰 <zhangjunjie@nantian.com.cn>
2022-09-06 09:31:00 +0800

    feat: 同步代码

---
+++ test-gemini/src/main/java/com/test/gemini/pojo/StudentDo.java
@@ -0,0 +1,14 @@
+package com.test.gemini.pojo;
+
+/**
+ * TODO
+ *
+ * @author: Junjie Zhang
+ * @date: 2022/8/26
+ */
+@Data
+public class StudentDo {
+    private String id;
+    private String name;
+    private Integer age;
+}
---
+++ test-gemini/src/main/java/com/test/gemini/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.test.gemini.service;
+
+/**
+ * TODO
+ *
+ * @author: Junjie Zhang
+ * @date: 2022/8/26
+ */
+public interface StudentService {
+
+    class Student {
+        private String id;
+        private String name;
+        private Integer age;
+    }
+
+}
